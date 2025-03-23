461e0feec6486ad9b44b9e73a221c9bed018d360
Suganya Ponnusamy <suganya.sugu88@gmail.com>
2025-03-22 23:06:10 -0500

    Assignment Completed

---
+++ create-array.js
@@ -0,0 +1,13 @@
+let groceryCart= ["Apple","Orange","Banana","Cherry","Blueberry"];// Array for 5 grocery store items.
+let numberValues=[1.1, 2.1, 2.3];// array of 3 numbers representing prices
+
+console. log("Grocery Cart: "+ groceryCart);//Display array 1
+console.log("Number Values: "+ numberValues);//Display array 2
+
+console.log("Grocery Cart Item:"+ groceryCart[1]);
+console.log("Grocery Cart Item:"+ groceryCart[3]);
+console.log("Grocery Cart Item:"+ groceryCart[4]);
+
+console.log("Price list for 1st Item:"+ numberValues[0]);
+console.log("Price list for 2nd Item:"+ numberValues[2]);
+
